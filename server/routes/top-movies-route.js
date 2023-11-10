ccb6b75fcdfa729a962a547b0874a608097304d5
Haver Ho <haverho.2020@gmail.com>
2023-11-09 22:30:05 -0800

    update

--- server/routes/top-movies-route.js
+++ server/routes/top-movies-route.js
@@ -86,6 +86,7 @@ module.exports = function (pool, app) {
                     const batch = movies.slice(i, i + batchsize);
 
                     const promises = batch.map(async (movie) => {
+                        console.log("MOVIE -", movie)
                         const movieObj = movie;
 
                         const starsQueryString = {
@@ -103,6 +104,8 @@ module.exports = function (pool, app) {
                     
                         const genreResult = await client.query(genresQueryString);
                         movieObj.movieGenres = genreResult.rows;
+
+                        console.log("MOVIE OBJECT -", movieObj)
                     
                         return movieObj;
                     })
