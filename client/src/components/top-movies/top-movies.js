e99492622df68459d2426e46827d62bedd0c6423
Haver Ho <haverho.2020@gmail.com>
2024-04-10 19:40:51 -0700

    Adjusted structure and styling of top movies

--- client/src/components/top-movies/top-movies.js
+++ client/src/components/top-movies/top-movies.js
@@ -2,7 +2,7 @@
 /* eslint-disable array-callback-return */
 import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { Link } from 'react-router-dom';
-// import './top-movies.css';
+import './top-movies.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { CartContext } from '../../contexts/CartContext';
@@ -74,33 +74,33 @@ const TopMovies = () => {
 
   return ( 
     <div className="top-movies-content w-full h-full py-[3%] px-[10%] text-center">
-      <h1 className='text-[#9EC8B9] text-bold'>Top 20 Rated Movies</h1>
-      <div className='grid-container w-full h-full flex-1 flex justify-center'>
+      <h1 className='text-[#9EC8B9] font-bold'>Top 20 Rated Movies</h1>
+      <div className='flex-container w-full h-full flex-1 flex justify-center'>
         {loading ? (
           <Loading/>
-        ): (<div className='movies-grid w-[250px] flex flex-wrap gap-[10px] sm:w-[510px] md:w-[770px] lg:w-[1030px] xl:w-[1290px] '>
+        ): (<div className='movies-grid w-[250px] flex flex-wrap gap-[10px]'>
           {top.map((item) => (
-            <div key={item.movieId}>
-              <Link key={item.movieId} to={`/single-movie?movieId=${item.movieId}`} className='grid-item w-[250px] h-[380px] rounded-[10px] transform duration-200 ease-in-out text-[#9EC8B9] no-underline text-[0.7em] flex flex-col items-center border-t-4 border-x-4 border-[#2E4F4F] border-solid hover:scale-[1.02]'>
+            <div key={item.movieId} className='grid-item w-[250px] h-[380px] rounded-[10px] transform duration-200 ease-in-out text-[#9EC8B9] no-underline text-[0.7em] flex flex-col items-center border-t-4 border-x-4 border-[#2E4F4F] border-solid hover:scale-[1.02]'>
+              <Link key={item.movieId} to={`/single-movie?movieId=${item.movieId}`} draggable='false' className='w-full'>
                 {item.moviePoster !== "N/A" 
                 ? <img className='poster w-full h-[250px] rounded-[10px] border border-solid border-black' src={item.moviePoster} alt="Movie Poster"></img>
                 : <div className='placeholderPoster flex justify-center items-center w-full h-[250px] rounded-[10px] border border-solid border-black bg-[rgb(212, 211, 211)]'>
                     <img className='w-[150px] h-[150px]' src={posterPlaceholder} alt="Movie Poster"></img>
                   </div>}
-                <div className='movie-info flex-1 w-full py-0 px-[5%] flex flex-col justify-evenly items-center'>
-                  <h1 className='text-[2em] text-center text-bold text-[#64CCC5]'>{item.movieTitle}</h1>
-                  <h2 className='w-full text-[1.5em] flex justify-between font-normal'>
-                    <span>{item.movieYear}</span>
-                    <span className='moviePrice text-bold text-[#DCF2F1]'>${item.moviePrice}</span>
-                    <span>{item.movieRating}
+              </Link>
+              <div className='movie-info flex-1 w-full py-0 px-[5%] flex flex-col justify-evenly items-center'>
+                  <h2 className='text-[2em] text-center font-bold text-[#64CCC5] cursor-text'>{item.movieTitle}</h2>
+                  <h3 className='w-full text-[1.5em] flex justify-between font-normal '>
+                    <span className='cursor-text'>{item.movieYear}</span>
+                    <span className='moviePrice font-bold text-[#DCF2F1] cursor-text'>${item.moviePrice}</span>
+                    <span className='cursor-text'>{item.movieRating}
                       <span className="fa-layers fa-fw">
                         <FontAwesomeIcon icon={faStar} color="#C5E898" size="sm"/>
                       </span>
                     </span>
-                  </h2>
+                  </h3>
                 </div>
-              </Link>
-              <button className='addToCart w-[200px] h-[30px] text-[#395B64] text-bold border-0 rounded-[20px] bg-[#A5C9CA] m-[3px] duration-200 hover:scale-[1.05] active:scale-[0.9]' name='addToCart'
+              <button className='addToCart w-[200px] h-[30px] text-[#395B64] text-base font-bold border-0 rounded-[20px] bg-[#A5C9CA] m-[3px] duration-200 hover:scale-[1.05] active:scale-[0.9]' name='addToCart'
                 onClick={() => cart.addOne(item.movieId, item.movieTitle)}>
                 ADD TO CART
               </button>
