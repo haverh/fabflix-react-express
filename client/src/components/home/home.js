0bbd501e6b1ff638af83d143c4d16e684e2479ec
Haver Ho <haverho.2020@gmail.com>
2024-04-10 19:42:02 -0700

    Adjusted styling to genre/startitle elements

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -2,7 +2,7 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
-// import './home.css';
+import './home.css';
 
 // const fetchURL = process.env.REACT_APP_VERCEL_FETCH_URL;
 const fetchURL = process.env.REACT_APP_LOCAL_FETCH_URL;
@@ -52,15 +52,15 @@ const Home = () => {
     
         for (let i = 0; i < 26; i++) {
             const alpha = String.fromCharCode(65 + i);
-            elements.push(<Link key={`alpha-${i}`} className='link-style textSelect no-underline text-[#2D9596] p-[5px] hover:text-[#56b3b4] hover:bg-[#064663] hover:rounded-[10px]' to={`/movies?startCharacter=${alpha}`}>{alpha}</Link>);
+            elements.push(<Link key={`alpha-${i}`} className='link-style' to={`/movies?startCharacter=${alpha}`}>{alpha}</Link>);
         }
 
         for (let i = 0; i < 10; i++) {
-            elements.push(<Link key={`number-${i}`} className='link-style textSelect no-underline text-[#2D9596] p-[5px] hover:text-[#56b3b4] hover:bg-[#064663] hover:rounded-[10px]' to={`/movies?startCharacter=${i}`}>{i}</Link>);
+            elements.push(<Link key={`number-${i}`} className='link-style' to={`/movies?startCharacter=${i}`}>{i}</Link>);
         }
     
         return (
-            <section className='w-full flex flex-col justify-center items-center text-center my-0 mb-[50px] ml-0'>
+            <section className='w-full flex flex-col m-3 justify-center items-center text-center'>
                 <h1>Browse by Movie Title</h1>
                 <div className="alphaBox grid text-center grid-cols-12 gap-[5px] text-bold rounded-[15px] bg-[#041C32] sm:gap-[10px] md:gap-[20px] lg:gap-[25px]">
                     {elements}
@@ -74,11 +74,11 @@ const Home = () => {
         const elements = [];
     
         for (let i = 0; i < genres.length; i++) {
-            elements.push(<Link key={genres[i].id} className='textSelect link-style no-underline text-[#2D9596] p-[5px] hover:text-[#56b3b4] hover:bg-[#064663] hover:rounded-[10px]' to={`/movies?genreId=${genres[i].id}`}>{genres[i].name}</Link>);
+            elements.push(<Link key={genres[i].id} className='link-style' to={`/movies?genreId=${genres[i].id}`}>{genres[i].name}</Link>);
         }
     
         return (
-            <section className='w-full flex flex-col justify-center items-center text-center my-0 mb-[50px] ml-0'>
+            <section className='w-full flex flex-col m-3 justify-center items-center text-center'>
                 <h1>Browse by Movie Genre</h1>
                 <div className="genreBox grid text-center grid-cols-3 gap-[5px] text-bold rounded-[15px] bg-[#041C32] sm:grid-cols-4 sm:gap-[5px] md:grid-cols-5 lg:grid-cols-6 lg:gap-[5px]">
                 
