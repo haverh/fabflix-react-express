635ff817edaddc2f2597bba1425ea7ccd30f0cba
Haver Ho <haverho.2020@gmail.com>
2024-04-10 21:35:18 -0700

    Modified folder name from 'login' to 'account'

---
+++ client/src/components/account/user.js
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const UserMenuImage = (
+  <FontAwesomeIcon icon={faUser} style={{"--fa-primary-color": "#b8efe7", "--fa-secondary-color": "#b8efe7",}} />
+)
+
+
+
+export default UserMenuImage;
