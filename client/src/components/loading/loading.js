3e5327d807572325a0daeb900afcac179123058b
Haver Ho <haverho.2020@gmail.com>
2024-04-15 13:44:02 -0700

    Changed styling of loading component

--- client/src/components/loading/loading.js
+++ client/src/components/loading/loading.js
@@ -3,7 +3,7 @@ import { faSpinner } from  '@fortawesome/free-solid-svg-icons';
 
 const Loading = () => {
   return (
-    <div className='w-full text-center text-2xl'><FontAwesomeIcon icon={faSpinner} /> Loading...</div>
+    <div className='w-full flex flex-row justify-center text-center text-2xl'><FontAwesomeIcon icon={faSpinner} size="2x" className="mr-2"/> <h1>Loading...</h1></div>
   )
 }
 
