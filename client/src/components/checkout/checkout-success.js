127f35f4af8a1ab52a10b95fde4d2acfaba3cadf
Haver Ho <haverho.2020@gmail.com>
2024-10-04 22:30:38 -0700

    Added check to see if redirected from stripe by checking sessionid

--- client/src/components/checkout/checkout-success.js
+++ client/src/components/checkout/checkout-success.js
@@ -1,128 +1,166 @@
-import { useState} from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
-
+import { useContext, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { CartContext } from '../../contexts/CartContext';
+import Loading from '../loading/loading';
 import fetchURL from '../../config';
 
 import './checkout-success.css';
 
 const CheckoutSuccess = () => {
 
-    const { user } = useAuth0();
-    console.log(localStorage)
-    const cart = JSON.parse(localStorage.getItem('cart'));
-    console.log(cart)
-    const tax = parseFloat(JSON.parse(localStorage.getItem('tax')).toFixed(2));
-    const total = parseFloat(JSON.parse(localStorage.getItem('total')).toFixed(2));
-    const grandTotal = parseFloat((total + tax).toFixed(2));
-    const email = user?.email;
-    const date = new Date().toISOString().slice(0,10);
-    const [orderId, setOrderId] = useState(null);
-    console.log(date)
-    // console.log(email)
+  const mycart = useContext(CartContext);
+  
+  const [cart, setCart] = useState(mycart.items);
+  const [tax, setTax] = useState(parseFloat((mycart.getTotalCost() * 0.1).toFixed(2)));
+  const [total, setTotal] = useState(parseFloat(mycart.getTotalCost().toFixed(2)));
+  const [grandTotal, setGrandTotal] = useState(parseFloat(mycart.getTotalCost() + tax).toFixed(2));
+  const [date, setDate] = useState(new Date().toISOString().slice(0,10));
+  const [orderId, setOrderId] = useState(null);
+  const [email, setEmail] = useState(null);
 
-    const documentOrder = async () => {
-        console.log("INSERTING INTO DATABASE")
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const session_id = queryParams.get("session_id");
+
+  const [loading, setLoading] = useState(true);
+  const [paymentStatus, setPaymentStatus] = useState(null);
+  
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetch(`${fetchURL}/api/authorization`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include",
+      });
+
+      if (response.status === 200) {
+        const jsonData = await response.json();
+        setEmail(jsonData.user.email);
+      }
+    }
+    getUser();
+  },[])
+
+
+  const documentOrder = async (session_id) => {
+    try {
+
+      const response = await fetch(`${fetchURL}/api/sale`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: "include",
+        body: JSON.stringify({cart, session_id, tax, total, grandTotal, email, date}),
+      });
+      const data = await response.json();
+      
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+
+  useEffect(() => {
+    if (session_id) {
+      const verifyPayment = async () => {
         try {
+          const response = await fetch(`${fetchURL}/api/payment-verify?session_id=${session_id}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            credentials: "include",
+          });
+          const data = await response.json();
 
-            const response = await fetch(`${fetchURL}/sale`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: "include",
-                body: JSON.stringify({cart, tax, total, grandTotal, email, date}),
-            });
-            const jsonData = await response.json();
-            setOrderId(jsonData);
-            console.log(jsonData);
+          if (data.success) {
+            setPaymentStatus('success');
+            const data = await documentOrder(session_id);
+            
+            mycart.clearCart();
+
+            setCart(data.cart);
+            setOrderId(data.saleId);
+            setTax(data.tax);
+            setTotal(data.total);
+            setGrandTotal(data.grandTotal);
+            setDate(data.date);
+
+          } else {
+            setPaymentStatus('failed');
+          }
         } catch (error) {
-            console.error('Error fetching data:', error);
+          console.error('Error verifying payment:', error);
+          setPaymentStatus('failed');
+        } finally {
+          setLoading(false);
         }
+      }
+      verifyPayment();
     }
+  }, [session_id])
 
-    if ( email !== undefined && email !== null && orderId === null ) { 
-        documentOrder();
-    }
+  if (loading) {
+    return <Loading />
+  }
 
+  if (paymentStatus === "success") {
 
     return (
-        
-        <div className='outerContainer'>
-            <h3 className='gratitude'>Thank you for your order</h3>
-            <div className='saleInfo'>
-                <>Order #: {orderId}</>
-                <br></br>
-                <div className='customerDetais'>
-                    <h4>Customer Details</h4>
-                    <><b>Email:</b> {email}</>
+      <div className='outerContainer'>
+        <h3 className='gratitude'>Thank you for your order</h3>
+        <div className='saleInfo'>
+          <>Order #: {orderId}</>
+          <br></br>
+          <div className='customerDetais'>
+            <h4>Customer Details</h4>
+            <><b>Email:</b> {email}</>
+          </div>
+          <br></br>
+          <div className='orderDetails'>
+            <h4>Order Details</h4>
+            <div className='order'>
+              <div className='orderRow'>
+                <div className='product'><b>Product</b></div>
+                <div className='quantity'><b>Quantity</b></div>
+                <div className='price'><b>Price</b></div>
+                <div className='total'><b>Total</b></div>
+              </div>
+              {cart && cart.map((item) => (
+                <div className='orderRow' key={item.id}>
+                  <div className='product'>{item.title}</div>
+                  <div className='quantity'>{item.quantity}</div>
+                  <div className='price'>${item.price}</div>
+                  <div className='total'>${(parseFloat(item.price) * parseInt(item.quantity)).toFixed(2)}</div>
                 </div>
-                <br></br>
-                <div className='orderDetails'>
-                    <h4>Order Details</h4>
-                    <div className='order'>
-                        <div className='orderRow'>
-                            <div className='product'><b>Product</b></div>
-                            <div className='quantity'><b>Quantity</b></div>
-                            <div className='price'><b>Price</b></div>
-                            <div className='total'><b>Total</b></div>
-                        </div>
-                        {cart.map((item) => (
-                            <div className='orderRow' key={item.id}>
-                                <div className='product'>{item.title}</div>
-                                <div className='quantity'>{item.quantity}</div>
-                                <div className='price'>${item.price}</div>
-                                <div className='total'>${(parseFloat(item.price) * parseInt(item.quantity)).toFixed(2)}</div>
-                            </div>
-                        ))}
-                    </div>
-                    <br></br>
-                    <div className='paymentDetails'>
-                        <h4>Payment Details</h4>
-                        <div className='paymentRow'>
-                            <div className='payment-type'><b>Sub-Total:</b></div>
-                            <div className='payment-amount'>${total.toFixed(2)}</div>
-                        </div>
-                        <div className='paymentRow'>
-                            <div className='payment-type'><b>Sales Tax:</b></div>
-                            <div className='payment-amount'>${tax}</div>
-                        </div>
-                        <div className='paymentRow'>
-                            <div className='payment-type'><b>Grand Total:</b></div>
-                            <div className='payment-amount'>${grandTotal}</div>
-                        </div>
-                    </div>
-                </div>
+              ))}
             </div>
-                    {/* <table className="cart-items table">
-                        <thead className="thead-light">
-                            <tr>
-                            <th scope="col" >Title</th>
-                            <th scope="col" >Price</th>
-                            <th scope="col" >Quantity</th>
-                            <th scope="col" >Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {cart.map((item) => (
-                            <tr key={item.id}>
-                                <td>{item.title}</td>
-                                <td>${item.price}</td>
-                                <td>{item.quantity}</td>
-                                <td>${(parseFloat(item.price) * parseInt(item.quantity)).toFixed(2)}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                    <div className='cart-bottom'>
-                    <div className='cart-checkout'>
-                        <div className='cart-total'>
-                            <p><b>Sub-Total:</b>  ${total.toFixed(2)}</p>
-                            <p><b>Sales Tax:</b> ${tax}</p>
-                            <p><b>Grand Total:</b> ${grandTotal}</p>
-                        </div>
-                    </div> */}
+            <br></br>
+            <div className='paymentDetails'>
+              <h4>Payment Details</h4>
+              <div className='paymentRow'>
+                <div className='payment-type'><b>Sub-Total:</b></div>
+                <div className='payment-amount'>${total}</div>
+              </div>
+              <div className='paymentRow'>
+                <div className='payment-type'><b>Sales Tax:</b></div>
+                <div className='payment-amount'>${tax}</div>
+              </div>
+              <div className='paymentRow'>
+                <div className='payment-type'><b>Grand Total:</b></div>
+                <div className='payment-amount'>${grandTotal}</div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     )
+  }
 }
 
 export default CheckoutSuccess;
