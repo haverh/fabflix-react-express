6831fe730520c97001dc09093e8a5dec1c6eb375
Haver Ho <haverho.2020@gmail.com>
2024-10-04 23:27:20 -0700

    Adjusted endpoint for authorization to include user instead of just role

--- server/routes/token-auth.js
+++ server/routes/token-auth.js
@@ -3,8 +3,9 @@ const middleware = require('../middleware/jwt_middleware')
 module.exports = function (pool, app) {
   app.get('/api/authorization', middleware.authenticateToken, async (req, res) => {
     try {
-      const role = middleware.authorizeToken(req, res);
-      res.status(200).json({ role: role });
+      const user = middleware.authorizeToken(req, res);
+      console.log(user)
+      res.status(200).json(user);
     } catch (error) {
       console.error('Error authorizing user:', error);
       res.status(403).json({ error: 'An error occurred' });
